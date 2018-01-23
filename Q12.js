fe13eb060c106a235b39cbc6f7cfffcf047ac25c
adrianacosta20 <aacosta@techlaunch.io>
2018-01-22 19:12:17 -0800

    first commit

---
+++ Q12.js
@@ -0,0 +1,11 @@
+// Write a while loop that prints to the console all the numbers from 10 to 1
+// in descending order. Use the code below in your solution.
+
+var num = 10;
+while ( 10) {
+  
+    num--;
+}
+console.log(num)
+
+// not done
---
+++ Q17.js
@@ -0,0 +1,8 @@
+// Create a for loop that prints all the even numbers from 1 to 20 to the console.
+// Use an if statement and the modulo operator in your solution.
+for ( i=2 ; i<=20 ; i+=2){
+    if(i % 2 == 0){
+        console.log(i)
+    }
+   
+}
